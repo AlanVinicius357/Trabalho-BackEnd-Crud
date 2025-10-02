5a7a652f1f763ca38a1bc663282570d4c34f6876
Wendels1 <ferreirawendel774@gmail.com>
2025-10-01 21:51:41 -0300

    Desenvolvi a rota de Crud de alunos parcialmente

--- routes/alunos.js
+++ routes/alunos.js
@@ -29,3 +29,40 @@ let listaAlunos = [
 router.get('/alunos', (req, res, next) => {
     res.json(listaAlunos);
 });
+
+router.get('/alunos/id', (req, res, next) => {
+    const id = req.params.id;
+
+    const aluno = listaAlunos.find(aluno => aluno.id == id);
+    if(!aluno){
+        return res.status(404).json({error: 'Aluno não encontrado'});
+    }
+    res.json(aluno);
+});
+
+// Criação de um novo aluno
+router.post('/alunos'), (req, res, next) => {
+    const { nome, matricula, idade, turma  } = req.body;
+
+    if(!nome || !matricula || !idade || !turma){
+        return res.status(400).json({error: 'Dados incompletos'});
+    }
+
+    if(listaAlunos.some(aluno => aluno.matricula === matricula)){
+        return res.status(409).json({error: 'Matrícula já existe'});
+    }
+
+    const novoAluno = {
+        id: listaAlunos.length + 1,
+        nome,
+        matricula,
+        idade,
+        turma,
+    };
+
+    listaAlunos.push(novoAluno);
+    res.status(201).json({message: "Aluno cadastrado com sucesso"},novoAluno);
+}
+
+
+module.exports = router;
